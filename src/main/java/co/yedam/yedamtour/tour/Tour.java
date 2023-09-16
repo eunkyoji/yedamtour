60dc83a4c4cbf269c545f80dd84f1fcc7b95d9fc
eunkyoji <eunkyo0226@gmail.com>
2023-09-16 10:12:09 +0900

    project all update

--- src/main/java/co/yedam/yedamtour/tour/Tour.java
+++ src/main/java/co/yedam/yedamtour/tour/Tour.java
@@ -1,4 +1,4 @@
-package co.yedam.yedamtour;
+package co.yedam.yedamtour.tour;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -9,19 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import co.yedam.yedamtour.common.ViewResolve;
 
-@WebServlet("/home.do")
-public class HomeController extends HttpServlet {
+@WebServlet("/tour.do")
+public class Tour extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public HomeController() {
+    public Tour() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String page = "main/main";
+		String page = "tour/tour";
 		ViewResolve.forward(request, response, page);
 	}
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
