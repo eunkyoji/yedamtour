3450df51440250b321f942ef5ee8c8d83048fc84
leehoy250 <ghdud4862@naver.com>
2023-09-26 15:42:39 +0900

    0926 이호영2

--- src/main/java/co/yedam/yedamtour/reservation/ReservationList2.java
+++ src/main/java/co/yedam/yedamtour/reservation/ReservationList2.java
@@ -29,21 +29,32 @@ public class ReservationList2 extends HttpServlet {
 		String startDate = request.getParameter("startDate");
 		String finishDate = request.getParameter("finishDate");
 		String start = request.getParameter("start");
-		String finish = request.getParameter("finish");
-		String startGrade = request.getParameter("startGrade");
+		String finish = request.getParameter("finish");		
 		String person = request.getParameter("person");
-		String startPrice = request.getParameter("startPrice");
-		String finishGrade = request.getParameter("finishGrade");
-		String finishPrice = request.getParameter("finishPrice");
 		
 		int direction = Integer.valueOf(request.getParameter("direction"));
 		int direction1 = Integer.valueOf(request.getParameter("direction1"));
 		
-		startvo.setReservationId(direction);;
+		startvo.setReservationId(direction);		
+		startvo = dao.reservationSelect(startvo);
+		if(startvo != null) {
+			request.setAttribute("startCart", startvo);
+			
+		}
 		finishvo.setReservationId(direction1);
+		finishvo = dao.reservationSelect(finishvo);
+		if(finishvo != null) {
+			request.setAttribute("finishCart", finishvo);
+		}
 		
+		int startCartId = startvo.getReservationId();
+		int finishCartId = startvo.getReservationId();
 		
-		
+		request.setAttribute("startDate", startDate);
+		request.setAttribute("finishDate", finishDate);
+		request.setAttribute("person", person);
+		request.setAttribute("startCartId", startCartId);
+		request.setAttribute("finishCartId", finishCartId);
 		
 		String page = "admin/reservation/reservationlist";
 		ViewResolve.forward(request, response, page);
